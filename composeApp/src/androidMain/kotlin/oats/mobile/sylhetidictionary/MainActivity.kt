be972a5442d771ef698faeb827cbd064c0218983
Malik Hasan <malik9hasan@gmail.com>
2024-11-28 21:03:51 -0800

    dependendcies

--- composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/MainActivity.kt
@@ -1,17 +1,15 @@
 package oats.mobile.sylhetidictionary
 
-import ui.app.App
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import org.koin.core.component.KoinComponent
+import ui.app.App
 
 class MainActivity : AppCompatActivity(), KoinComponent {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent { App() }
     }
 }
