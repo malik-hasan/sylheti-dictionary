6de0b20b760bba9b109d1104ac98f4c6fb6199ed
Malik Hasan <malik9hasan@gmail.com>
2025-03-25 20:57:00 -0700

    desktop scrollbar

--- composeApp/src/desktopMain/kotlin/oats/mobile/sylhetidictionary/ui/components/ScrollBar.desktop.kt
+++ composeApp/src/desktopMain/kotlin/oats/mobile/sylhetidictionary/ui/components/ScrollBar.desktop.kt
@@ -2,9 +2,20 @@ package oats.mobile.sylhetidictionary.ui.components
 
 import androidx.compose.foundation.ScrollbarAdapter
 import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+import oats.mobile.sylhetidictionary.ui.utils.ifTrue
 
 @Composable
 actual fun ScrollBar(
@@ -13,5 +24,16 @@ actual fun ScrollBar(
     density: Density,
     scrollingFromScrollBar: () -> Unit
 ) {
-    VerticalScrollbar(ScrollbarAdapter(lazyListState))
+    val interactionSource = remember { MutableInteractionSource() }
+    val isHovered by interactionSource.collectIsHoveredAsState()
+    val isDragged by interactionSource.collectIsDraggedAsState()
+
+    VerticalScrollbar(
+        adapter = ScrollbarAdapter(lazyListState),
+        modifier = Modifier
+            .ifTrue(isHovered || isDragged) {
+                background(MaterialTheme.colorScheme.surfaceContainer)
+            }.padding(4.dp),
+        interactionSource = interactionSource
+    )
 }
