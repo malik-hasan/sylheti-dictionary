6c505d624aaf1a136af3d5bfd15a9c8744a43797
Malik Hasan <malik9hasan@gmail.com>
2025-04-06 19:18:00 -0700

    ignore ime padding for scrollbar

--- composeApp/src/commonMain/kotlin/oats/mobile/sylhetidictionary/ui/screens/search/search/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/oats/mobile/sylhetidictionary/ui/screens/search/search/SearchScreen.kt
@@ -14,9 +14,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -324,7 +328,7 @@ fun SearchScreen(
                         enter = slideInHorizontally { it },
                         exit = slideOutHorizontally { it },
                         modifier = Modifier
-                            .padding(bottom = scaffoldPadding.calculateBottomPadding())
+                            .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Bottom))
                             .align(Alignment.CenterVertically)
                     ) {
                         ScrollBar(
