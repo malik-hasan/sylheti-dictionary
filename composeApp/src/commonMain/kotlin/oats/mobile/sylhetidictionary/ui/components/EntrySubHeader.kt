9297a1bbfe20291909d586cd412d3dd09aa6bf4d
Malik Hasan <malik9hasan@gmail.com>
2025-10-18 09:42:26 -0700

    icon rotation

--- composeApp/src/commonMain/kotlin/oats/mobile/sylhetidictionary/ui/components/EntrySubHeader.kt
+++ composeApp/src/commonMain/kotlin/oats/mobile/sylhetidictionary/ui/components/EntrySubHeader.kt
@@ -1,5 +1,6 @@
 package oats.mobile.sylhetidictionary.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -7,19 +8,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.painterResource
+import sylhetidictionary.composeapp.generated.resources.Res
+import sylhetidictionary.composeapp.generated.resources.keyboard_arrow_down
 
 @Composable
 fun EntrySubHeader(
@@ -45,9 +48,11 @@ fun EntrySubHeader(
             color = MaterialTheme.colorScheme.onSecondaryContainer
         )
 
+        val rotation by animateFloatAsState(if (expanded) 180f else 0f)
         Icon(
-            if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
-            "expand"
+            painter = painterResource(Res.drawable.keyboard_arrow_down),
+            contentDescription = "Expand",
+            modifier = Modifier.rotate(rotation)
         )
     }
 }
