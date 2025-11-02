09316ebffafe30d6cf2c7089720f01bb2f25843d
Malik Hasan <malik9hasan@gmail.com>
2025-11-01 22:29:08 -0700

    bring back edge to edge

--- composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/MainActivity.kt
@@ -2,6 +2,7 @@ package oats.mobile.sylhetidictionary
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import oats.mobile.sylhetidictionary.ui.app.App
 import org.koin.core.component.KoinComponent
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity(), KoinComponent {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent { App() }
     }
 }
