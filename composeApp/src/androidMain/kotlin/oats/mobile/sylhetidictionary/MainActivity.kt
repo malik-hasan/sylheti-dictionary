6ce0b0dea237dcf66ee5bec0eefdb5e552758830
Malik Hasan <malik9hasan@gmail.com>
2025-11-02 08:48:59 -0800

    fix edge to edge system bar theming

--- composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/MainActivity.kt
@@ -1,17 +1,44 @@
 package oats.mobile.sylhetidictionary
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import oats.mobile.sylhetidictionary.ui.app.App
+import oats.mobile.sylhetidictionary.ui.app.AppViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.component.KoinComponent
 
 class MainActivity : AppCompatActivity(), KoinComponent {
 
+    private val vm: AppViewModel by viewModel()
+
+    // copied from EdgeToEdge
+    private val defaultLightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+    private val defaultDarkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent { App() }
+        setContent {
+            val theme by vm.theme.collectAsStateWithLifecycle()
+            val darkTheme = theme.isDarkTheme()
+            LaunchedEffect(darkTheme) {
+                enableEdgeToEdge(
+                    statusBarStyle = if (darkTheme) {
+                        SystemBarStyle.dark(Color.TRANSPARENT)
+                    } else SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+                    navigationBarStyle = if (darkTheme) {
+                        SystemBarStyle.dark(defaultDarkScrim)
+                    } else SystemBarStyle.light(defaultLightScrim, defaultDarkScrim)
+                )
+            }
+
+            App()
+        }
     }
 }
--- composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/ui/theme/SDTheme.android.kt
+++ composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/ui/theme/SDTheme.android.kt
@@ -1,39 +1,26 @@
 package oats.mobile.sylhetidictionary.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 @Composable
 actual fun SDTheme(
     dynamicTheme: Boolean,
     darkTheme: Boolean,
     content: @Composable () -> Unit
-) {
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            WindowCompat.getInsetsController((view.context as Activity).window, view).isAppearanceLightStatusBars = !darkTheme
+) = SDTheme(
+    colorScheme = when {
+        // Dynamic color is available on Android 12+
+        dynamicTheme && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-    }
 
-    SDTheme(
-        colorScheme = when {
-            // Dynamic color is available on Android 12+
-            dynamicTheme && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> darkScheme
-            else -> lightScheme
-        },
-        content = content
-    )
-}
+        darkTheme -> darkScheme
+        else -> lightScheme
+    },
+    content = content
+)
