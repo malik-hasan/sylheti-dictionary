b13ea40e4a6b3a37424384437936707f0635a3fb
Malik Hasan <malik9hasan@gmail.com>
2025-12-08 08:44:10 -0800

    delete backhandler

--- composeApp/src/androidMain/kotlin/oats/mobile/sylhetidictionary/ui/components/BackHandler.android.kt
+++
@@ -1,7 +0,0 @@
-package oats.mobile.sylhetidictionary.ui.components
-
-import androidx.compose.runtime.Composable
-
-@Composable
-actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) =
-    androidx.activity.compose.BackHandler(enabled, onBack)
--- composeApp/src/commonMain/kotlin/oats/mobile/sylhetidictionary/ui/components/BackHandler.kt
+++
@@ -1,6 +0,0 @@
-package oats.mobile.sylhetidictionary.ui.components
-
-import androidx.compose.runtime.Composable
-
-@Composable
-expect fun BackHandler(enabled: Boolean = true, onBack: () -> Unit)
