ede9d4be1ef6835f4085e84943fd531f79030744
Malik Hasan <malik9hasan@gmail.com>
2025-12-13 12:26:09 -0800

    seach sugg alignment

--- composeApp/src/commonMain/kotlin/oats/mobile/sylhetidictionary/ui/components/SearchSuggestion.kt
+++ composeApp/src/commonMain/kotlin/oats/mobile/sylhetidictionary/ui/components/SearchSuggestion.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
@@ -26,10 +24,10 @@ fun SearchSuggestion(
         modifier = Modifier
             .clickable(onClick = onClick)
             .then(modifier)
-            .padding(horizontal = 16.dp)
-            .fillMaxWidth()
-            .heightIn(min = 32.dp),
-        verticalAlignment = Alignment.CenterVertically,
+            .padding(
+                horizontal = 16.dp,
+                vertical = 4.dp
+            ).fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         icon()
