8816648779391ca24969e93fa1b8956d44d60c39
Malik Hasan <malik9hasan@gmail.com>
2025-12-24 01:59:52 -0800

    robust with mutex and cancellations

--- composeApp/src/desktopMain/kotlin/oats/mobile/sylhetidictionary/ui/utils/AudioPlayer.kt
+++ composeApp/src/desktopMain/kotlin/oats/mobile/sylhetidictionary/ui/utils/AudioPlayer.kt
@@ -7,29 +7,36 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import oats.mobile.sylhetidictionary.di.utils.injectLogger
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import java.net.HttpURLConnection
 import java.net.URI
 import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.Clip
 import javax.sound.sampled.LineEvent
 import kotlin.coroutines.cancellation.CancellationException
 
-actual class AudioPlayer: KoinComponent {
+actual class AudioPlayer : KoinComponent {
 
     private val logger by injectLogger()
     private val scope: CoroutineScope by inject()
 
+    private val clipMutex = Mutex()
     private var playbackJob: Job? = null
-    private var audioClip: Clip? = null
+    private var clip: Clip? = null
 
     actual fun play(url: String) {
-        release()
+        playbackJob?.cancel()
         playbackJob = scope.launch(Dispatchers.IO) {
+            var audioInputStream: AudioInputStream? = null
+            var decodedAudioInputStream: AudioInputStream? = null
             try {
+                logger.d("AudioPlayer: loading $url")
                 ensureActive()
                 val connection = (URI(url).toURL().openConnection() as HttpURLConnection).apply {
                     setRequestProperty("User-Agent", "SylhetiDictionary.app")
@@ -37,54 +44,71 @@ actual class AudioPlayer: KoinComponent {
                 }
 
                 ensureActive()
-                val audioInputStream = AudioSystem.getAudioInputStream(connection.inputStream)
+                audioInputStream = AudioSystem.getAudioInputStream(connection.inputStream)
                 val decodedFormat = audioInputStream.format.run {
                     AudioFormat(
                         AudioFormat.Encoding.PCM_SIGNED,
                         sampleRate,
-                        16,  // 16-bit audio
+                        16,
                         channels,
-                        channels * 2,  // 2 bytes per sample (16-bit)
+                        channels * 2,
                         sampleRate,
-                        false  // little-endian
+                        false
                     )
                 }
 
                 ensureActive()
-                val decodedStream = AudioSystem.getAudioInputStream(decodedFormat, audioInputStream)
+                decodedAudioInputStream = AudioSystem.getAudioInputStream(decodedFormat, audioInputStream)
 
-                ensureActive()
-                audioClip = AudioSystem.getClip().apply {
-                    open(decodedStream)
-
-                    val playbackComplete = CompletableDeferred<Unit>(coroutineContext.job)
-                    addLineListener { event ->
-                        if (event.type == LineEvent.Type.STOP) {
-                            playbackComplete.complete(Unit)
+                val completion = CompletableDeferred<Unit>(coroutineContext.job)
+                clipMutex.withLock {
+                    stopClip()
+                    clip = AudioSystem.getClip()
+                    clip?.run {
+                        addLineListener { event ->
+                            if (event.type == LineEvent.Type.STOP) {
+                                completion.complete(Unit)
+                            }
                         }
+
+                        ensureActive()
+                        open(decodedAudioInputStream)
+
+                        ensureActive()
+                        logger.d("AudioPlayer: starting")
+                        start()
+
                     }
-
-                    logger.d("AudioPlayer: starting")
-                    start()
-                    playbackComplete.await()
-                    logger.d("AudioPlayer: finished")
                 }
-            } catch (e: Exception) {
-                if (e is CancellationException) {
-                    logger.d("AudioPlayer: cancelled")
-                    throw e
-                } else logger.e("AudioPlayer: ${e.message}")
-            }
-        }
 
-        playbackJob?.invokeOnCompletion {
-            logger.d("AudioPlayer: closing")
-            audioClip?.close()
-            audioClip = null
+                completion.await()
+                logger.d("AudioPlayer: finished")
+            } catch (e: CancellationException) {
+                logger.d("AudioPlayer: cancelled")
+                throw e
+            } catch (e: Exception) {
+                logger.e("AudioPlayer error: ${e.message}")
+            } finally {
+                clipMutex.withLock { stopClip() }
+                audioInputStream?.close()
+                decodedAudioInputStream?.close()
+            }
         }
     }
 
+
     actual fun release() {
         playbackJob?.cancel()
+        playbackJob = null
+    }
+
+    private fun stopClip() {
+        clip?.run {
+            logger.d("AudioPlayer: stopping")
+            stop()
+            flush()
+            close()
+        }
+        clip = null
     }
 }
