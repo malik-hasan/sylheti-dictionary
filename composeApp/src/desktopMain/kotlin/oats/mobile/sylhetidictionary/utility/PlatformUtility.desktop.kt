4bdaedc52cee33f701dd4d8ecc3e9d3a482c6977
Malik Hasan <malik9hasan@gmail.com>
2026-01-10 22:33:49 -0800

    simplify

--- composeApp/src/desktopMain/kotlin/oats/mobile/sylhetidictionary/utility/PlatformUtility.desktop.kt
+++ composeApp/src/desktopMain/kotlin/oats/mobile/sylhetidictionary/utility/PlatformUtility.desktop.kt
@@ -7,15 +7,15 @@ val databaseDirectory by lazy {
     val userHome = System.getProperty("user.home")
     when {
         os.contains("mac") -> File(userHome, "Library/Application Support/SylhetiDictionary")
-        os.contains("win") -> {
-            val appData = System.getenv("LOCALAPPDATA") ?: "$userHome\\AppData\\Local"
-            File(appData, "SylhetiDictionary")
-        }
+        os.contains("win") -> File(
+            System.getenv("LOCALAPPDATA") ?: "$userHome\\AppData\\Local",
+            "SylhetiDictionary"
+        )
 
-        else -> {
-            val xdgDataHome = System.getenv("XDG_DATA_HOME") ?: "$userHome/.local/share"
-            File(xdgDataHome, "sylhetidictionary")
-        }
+        else -> File(
+            System.getenv("XDG_DATA_HOME") ?: "$userHome/.local/share",
+            "sylhetidictionary"
+        )
     }.apply { mkdirs() }
 }
 
